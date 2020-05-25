59dd439fe0558cbdda693ca107950a72179bf03f
Kirankumar Sripati <krnsripati@gmail.com>
2020-05-25 23:34:06 +0530

    Add PostsList for category route

--- frontend/src/components/Category.tsx
+++ frontend/src/components/Category.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import { Segment, Header } from 'semantic-ui-react';
+import PostsList from './PostsList';
 
-const Category = () => <h1>Category</h1>
+const Category = ({ match }) => {
+  const { category } = match.params;
+  return (
+    <Segment.Group>
+      <Segment>
+        <Header>{category}</Header>
+      </Segment>
+      <PostsList category={category} />
+    </Segment.Group>
+  )
+}
 
 export default Category
